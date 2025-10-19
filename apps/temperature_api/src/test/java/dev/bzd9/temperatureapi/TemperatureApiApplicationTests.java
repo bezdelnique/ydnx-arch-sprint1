ce6761c371b5b4a8a88a2974cbeed2457ca8f64d
Vitaly Belenky <v.belenkij@sigma-it.ru>
2025-10-20 00:00:49 +0300

    Add Temperature API APP

---
+++ apps/temperature_api/src/test/java/dev/bzd9/temperatureapi/TemperatureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bzd9.temperatureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
